20c94b8d0ccbcf10cafb27ec8e180ae86ea73cf7
Karate Yuan <haodong_yuan@163.com>
2020-05-29 13:45:39 +0800

    log fisrt commit

---
+++ kara/base/LogFile.cpp
@@ -0,0 +1,44 @@
+/**
+ * @Author Karate Yuan
+ * @Email haodong_yuan@163.com
+ * @Date: 2020/5/29
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "LogFile.h"
+#include "FileUtil.h"
+
+using namespace std;
+
+LogFile::LogFile(const string& basename, int flushEveryN)
+    : basename_(basename),
+      flushEveryN_(flushEveryN),
+      count_(0),
+      mutex_(new MutexLock)
+{
+    // unique_ptr api
+    file_.reset(new AppendFile(basename));
+}
+
+void LogFile::append(const char* logline, int len){
+    MutexLockGuard lock(*mutex_);
+    append_unlocked(logline, len);
+}
+
+void LogFile::flush(){
+    MutexLockGuard lock(*mutex_);
+    // 指针 api
+    file_ -> flush();
+}
+
+void LogFile::append_unlocked(const char* logline, int len){
+    file_ -> append(logline, len);
+    ++count_;
+    if(count_ >= flushEveryN_){
+        count_ = 0;
+        file_ -> flush();
+    }
+}
